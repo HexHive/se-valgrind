62bdd56774e81998a7dd6b9c003de94226d28ec7
Derrick McKee <derrick.mckee@gmail.com>
2020-03-05 10:58:01 -0500

    reformatting

--- segrind/clo.c
+++ segrind/clo.c
@@ -2,39 +2,36 @@
 // Created by derrick on 3/4/20.
 //
 
-#include "se.h"
 #include "pub_tool_options.h"
+#include "se.h"
 
-Bool SE_(process_cmd_line_option)(const HChar* arg) {
-    const HChar *tmp_str;
-    if(VG_STR_CLO(arg, "--in-pipe", tmp_str)) {
-      if ((SE_(cmd_in) = VG_(fd_open)(tmp_str, VKI_O_RDONLY, 0)) < 0) {
-        VG_(fmsg_bad_option(arg, "Could not open in-pipe\n"));
-      }
-    } else if(VG_STR_CLO(arg, "--out-pipe", tmp_str)) {
-      if ((SE_(cmd_out) = VG_(fd_open)(tmp_str, VKI_O_WRONLY, 0)) < 0) {
-        VG_(fmsg_bad_option(arg, "Could not open out-pipe\n"));
-      }
-    } else if(VG_STR_CLO(arg, "--log", tmp_str)) {
-      if ((SE_(log) = VG_(fd_open)(tmp_str, VKI_O_WRONLY, 0)) < 0) {
-        VG_(fmsg_bad_option(arg, "Could not open log\n"));
-      }
+Bool SE_(process_cmd_line_option)(const HChar *arg) {
+  const HChar *tmp_str;
+  if (VG_STR_CLO(arg, "--in-pipe", tmp_str)) {
+    if ((SE_(cmd_in) = VG_(fd_open)(tmp_str, VKI_O_RDONLY, 0)) < 0) {
+      VG_(fmsg_bad_option(arg, "Could not open in-pipe\n"));
     }
+  } else if (VG_STR_CLO(arg, "--out-pipe", tmp_str)) {
+    if ((SE_(cmd_out) = VG_(fd_open)(tmp_str, VKI_O_WRONLY, 0)) < 0) {
+      VG_(fmsg_bad_option(arg, "Could not open out-pipe\n"));
+    }
+  } else if (VG_STR_CLO(arg, "--log", tmp_str)) {
+    if ((SE_(log) = VG_(fd_open)(tmp_str, VKI_O_WRONLY, 0)) < 0) {
+      VG_(fmsg_bad_option(arg, "Could not open log\n"));
+    }
+  }
 
-    return False;
+  return False;
 }
 
 void SE_(print_usage)(void) {
-    VG_(printf)(
-            "--in-pipe=<file>    Filename of the command server read pipe\n"
-            "--out-pipe=<file>   Filename of the command server write pipe\n"
-            "--log=<file>        Filename of the log to write to\n"
-            );
+  VG_(printf)
+  ("--in-pipe=<file>    Filename of the command server read pipe\n"
+   "--out-pipe=<file>   Filename of the command server write pipe\n"
+   "--log=<file>        Filename of the log to write to\n");
 }
 
-void SE_(print_debug_usage)(void) {
-    SE_(print_usage)();
-}
+void SE_(print_debug_usage)(void) { SE_(print_usage)(); }
 
 void SE_(set_clo_defaults)(void) {
   SE_(cmd_in) = -1;
--- segrind/se_main.c
+++ segrind/se_main.c
@@ -36,56 +36,54 @@ static ThreadId target_id = VG_INVALID_THREADID;
 
 static void SE_(post_clo_init)(void) {}
 
-static
-void SE_(thread_creation)(ThreadId tid, ThreadId child) {
-    if(!client_running) {
-        VG_(umsg)("Thread %u created target thread %u\n", tid, child);
-        target_id = child;
-    }
+static void SE_(thread_creation)(ThreadId tid, ThreadId child) {
+  if (!client_running) {
+    VG_(umsg)("Thread %u created target thread %u\n", tid, child);
+    target_id = child;
+  }
 }
 
-static
-void SE_(thread_exit)(ThreadId tid) {
-    if(client_running && tid == target_id) {
-        VG_(umsg)("Thread %u has exited\n", tid);
-        client_running = False;
-        target_id = VG_INVALID_THREADID;
-    }
+static void SE_(thread_exit)(ThreadId tid) {
+  if (client_running && tid == target_id) {
+    VG_(umsg)("Thread %u has exited\n", tid);
+    client_running = False;
+    target_id = VG_INVALID_THREADID;
+  }
 }
 
-static
-void SE_(start_client_code)(ThreadId tid, ULong blocks_dispatched)
-{
-    if(!client_running && tid == target_id) {
-        VG_(umsg)("Thread %u is starting executing at instruction 0x%lx with blocks_dispatched=%llu\n", tid, VG_(get_IP)(tid), blocks_dispatched);
-        client_running = True;
-        VG_(umsg)("Thread %u is about to call target function\n", tid);
-        VG_(set_IP)(tid, (Addr)0x401145);
-        VG_(umsg)("Thread %u returned\n", tid);
-    }
+static void SE_(start_client_code)(ThreadId tid, ULong blocks_dispatched) {
+  if (!client_running && tid == target_id) {
+    VG_(umsg)
+    ("Thread %u is starting executing at instruction 0x%lx with "
+     "blocks_dispatched=%llu\n",
+     tid, VG_(get_IP)(tid), blocks_dispatched);
+    client_running = True;
+    VG_(umsg)("Thread %u is about to call target function\n", tid);
+    VG_(set_IP)(tid, (Addr)0x401145);
+    VG_(umsg)("Thread %u returned\n", tid);
+  }
 }
 
-static
-void SE_(stop_client_code)(ThreadId tid, ULong blocks_dispatched)
-{
-//    VG_(umsg)("Thread %u stopped executing at instruction 0x%lx with blocks_dispatched=%llu\n", tid, VG_(get_IP)(tid), blocks_dispatched);
+static void SE_(stop_client_code)(ThreadId tid, ULong blocks_dispatched) {
+  //    VG_(umsg)("Thread %u stopped executing at instruction 0x%lx with
+  //    blocks_dispatched=%llu\n", tid, VG_(get_IP)(tid), blocks_dispatched);
 }
 
-static
-IRSB* SE_(instrument) ( VgCallbackClosure* closure,
-                      IRSB* bb,
-                      const VexGuestLayout* layout, 
-                      const VexGuestExtents* vge,
-                      const VexArchInfo* archinfo_host,
-                      IRType gWordTy, IRType hWordTy )
-{
-//    if(target_function_running) {
-        DiEpoch de = VG_(current_DiEpoch)();
-        const HChar *fnname = "@UNKNOWN@";
-        VG_(get_fnname)(de, closure->nraddr, &fnname);
-        VG_(umsg)("Thread %u requested translation of block starting at 0x%lx in function %s\n", closure->tid, closure->nraddr, fnname);
-//    }
-    return bb;
+static IRSB *SE_(instrument)(VgCallbackClosure *closure, IRSB *bb,
+                             const VexGuestLayout *layout,
+                             const VexGuestExtents *vge,
+                             const VexArchInfo *archinfo_host, IRType gWordTy,
+                             IRType hWordTy) {
+  //    if(target_function_running) {
+  DiEpoch de = VG_(current_DiEpoch)();
+  const HChar *fnname;
+  VG_(get_fnname)(de, closure->nraddr, &fnname);
+  VG_(umsg)
+  ("Thread %u requested translation of block starting at 0x%lx in function "
+   "%s\n",
+   closure->tid, closure->nraddr, fnname);
+  //    }
+  return bb;
 }
 
 static void SE_(fini)(Int exitcode) {
@@ -102,31 +100,27 @@ static void SE_(fini)(Int exitcode) {
   }
 }
 
-static void SE_(pre_clo_init)(void)
-{
-   VG_(details_name)            ("Software Ethology");
-   VG_(details_version)         (NULL);
-   VG_(details_description)     ("The binary analysis tool");
-   VG_(details_copyright_author)(
-      "Copyright (C) 2020, and GNU GPL'd, by Derrick McKee.");
-   VG_(details_bug_reports_to)  ("derrick@geth.systems");
+static void SE_(pre_clo_init)(void) {
+  VG_(details_name)("Software Ethology");
+  VG_(details_version)(NULL);
+  VG_(details_description)("The binary analysis tool");
+  VG_(details_copyright_author)
+  ("Copyright (C) 2020, and GNU GPL'd, by Derrick McKee.");
+  VG_(details_bug_reports_to)("derrick@geth.systems");
 
-   VG_(details_avg_translation_sizeB) ( 275 );
+  VG_(details_avg_translation_sizeB)(275);
 
-   VG_(basic_tool_funcs)        (SE_(post_clo_init),
-                                 SE_(instrument),
-                                 SE_(fini));
+  VG_(basic_tool_funcs)(SE_(post_clo_init), SE_(instrument), SE_(fini));
 
-    VG_(needs_command_line_options)(SE_(process_cmd_line_option),
-                                    SE_(print_usage),
-                                    SE_(print_debug_usage));
+  VG_(needs_command_line_options)
+  (SE_(process_cmd_line_option), SE_(print_usage), SE_(print_debug_usage));
 
-    VG_(track_start_client_code)(SE_(start_client_code));
-    VG_(track_stop_client_code)(SE_(stop_client_code));
-    VG_(track_pre_thread_ll_create)(SE_(thread_creation));
-    VG_(track_pre_thread_ll_exit)(SE_(thread_exit));
+  VG_(track_start_client_code)(SE_(start_client_code));
+  VG_(track_stop_client_code)(SE_(stop_client_code));
+  VG_(track_pre_thread_ll_create)(SE_(thread_creation));
+  VG_(track_pre_thread_ll_exit)(SE_(thread_exit));
 
-    SE_(set_clo_defaults)();
+  SE_(set_clo_defaults)();
 }
 
 VG_DETERMINE_INTERFACE_VERSION(SE_(pre_clo_init))
