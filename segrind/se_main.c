744807d0d360cdb3adf3fc1f4f424a45698172f3
Derrick McKee <derrick.mckee@gmail.com>
2020-04-13 17:41:49 -0400

    enabling a disabled feature

--- segrind/se_main.c
+++ segrind/se_main.c
@@ -583,9 +583,9 @@ static void record_current_state(Addr addr) {
   //  VG_(umsg)
   //      ("Executing 0x%lx (%s)\n", VG_(get_IP)(target_id), fnname);
   if (client_running && main_replaced && target_called) {
-    //    if (VG_(sizeXA)(program_states) > SE_(MaxInstructions)) {
-    //      SE_(report_too_many_instrs_to_commander)();
-    //    }
+    if (VG_(sizeXA)(program_states) > SE_(MaxInstructions)) {
+      SE_(report_too_many_instrs_to_commander)();
+    }
 
     VexGuestArchState current_state;
     VG_(get_shadow_regs_area)
